fa6b39b58e8db29ab4178829cc559a05efd0d312
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-05-31 15:14:27 +0100

    Add ActionExporter to the tool/DDL

--- build_groundzero_ddl.sh
+++ build_groundzero_ddl.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 rm -rf groundzero_ddl
-rm -rf src/main/java/uk/gov/ons/census/casesvc
+rm -rf git_cloned_src
 
 mkdir temp_clone
 cd temp_clone
 git clone https://github.com/ONSdigital/census-rm-case-processor.git
 git clone https://github.com/ONSdigital/census-rm-action-scheduler.git
+git clone https://github.com/ONSdigital/census-rm-actionexporter-service.git
 cd ..
 
 mkdir -p git_cloned_src/uk/gov/ons/census/casesvc/model/entity
@@ -13,6 +14,9 @@ cp temp_clone/census-rm-case-processor/src/main/java/uk/gov/ons/census/casesvc/m
 
 mkdir -p git_cloned_src/uk/gov/ons/census/action/model/entity
 cp temp_clone/census-rm-action-scheduler/src/main/java/uk/gov/ons/census/action/model/entity/* git_cloned_src/uk/gov/ons/census/action/model/entity
+
+mkdir -p git_cloned_src/uk/gov/ons/ctp/response/action/export/domain
+cp temp_clone/census-rm-actionexporter-service/src/main/java/uk/gov/ons/ctp/response/action/export/domain/* git_cloned_src/uk/gov/ons/ctp/response/action/export/domain
 
 rm -rf temp_clone
 
@@ -22,3 +26,4 @@ rm -rf git_cloned_src
 
 java -jar target/census-rm-ddl-1.0-SNAPSHOT.jar casev2 uk.gov.ons.census.casesvc.model.entity
 java -jar target/census-rm-ddl-1.0-SNAPSHOT.jar actionv2 uk.gov.ons.census.action.model.entity
+java -jar target/census-rm-ddl-1.0-SNAPSHOT.jar actionexporter uk.gov.ons.ctp.response.action.export.domain
---
+++ groundzero_ddl/actionexporter.sql
@@ -0,0 +1,110 @@
+create sequence actionexporter.actionrequestpkseq start 1 increment 1;
+create sequence actionexporter.contactpkseq start 1 increment 1;
+
+    create table actionexporter.actionrequest (
+       actionrequestpk int4 not null,
+        actionid uuid,
+        actionplanname varchar(255),
+        actiontypename varchar(255),
+        casegroupstatus varchar(255),
+        caseid uuid,
+        caseref varchar(255),
+        datestored timestamp,
+        enrolmentstatus varchar(255),
+        exerciseref varchar(255),
+        exportjobid uuid,
+        iac varchar(255),
+        iac_wales varchar(255),
+        legalbasis varchar(255),
+        priority varchar(255),
+        qid varchar(255),
+        qid_wales varchar(255),
+        questionset varchar(255),
+        region varchar(255),
+        respondentstatus varchar(255),
+        responserequired boolean,
+        returnbydate varchar(255),
+        sampleunitref varchar(255),
+        surveyref varchar(255),
+        addressfk uuid,
+        contactfk int4,
+        primary key (actionrequestpk)
+    );
+
+    create table actionexporter.address (
+       addresspk uuid not null,
+        addresstype varchar(255),
+        category varchar(255),
+        country varchar(255),
+        estabtype varchar(255),
+        lad varchar(255),
+        latitude numeric(19, 2),
+        address_line1 varchar(255),
+        address_line2 varchar(255),
+        address_line3 varchar(255),
+        locality varchar(255),
+        longitude numeric(19, 2),
+        organisationName varchar(255),
+        postcode varchar(255),
+        townName varchar(255),
+        primary key (addresspk)
+    );
+
+    create table actionexporter.contact (
+       contactpk int4 not null,
+        emailaddress varchar(255),
+        forename varchar(255),
+        phonenumber varchar(255),
+        surname varchar(255),
+        title varchar(255),
+        primary key (contactpk)
+    );
+
+    create table actionexporter.exportfile (
+       id uuid not null,
+        datesuccessfullysent timestamp,
+        exportjobid uuid,
+        filename varchar(255),
+        status varchar(255),
+        primary key (id)
+    );
+
+    create table actionexporter.exportjob (
+       id uuid not null,
+        primary key (id)
+    );
+
+    create table actionexporter.filerowcount (
+       filename varchar(255) not null,
+        datesent timestamp,
+        reported boolean not null,
+        rowcount int4 not null,
+        sendresult boolean,
+        primary key (filename)
+    );
+
+    create table actionexporter.template (
+       templatenamepk varchar(255) not null,
+        content varchar(255),
+        datemodified timestamp,
+        primary key (templatenamepk)
+    );
+
+    create table actionexporter.templatemapping (
+       actiontypenamepk varchar(255) not null,
+        datemodified timestamp,
+        filenameprefix varchar(255),
+        requesttype varchar(255),
+        templatenamefk varchar(255),
+        primary key (actiontypenamepk)
+    );
+
+    alter table if exists actionexporter.actionrequest 
+       add constraint FKkp38rwvx4kqx83ev6axub9010 
+       foreign key (addressfk) 
+       references actionexporter.address;
+
+    alter table if exists actionexporter.actionrequest 
+       add constraint FKcm2uhgra4r3l5bhmqi0qbysmg 
+       foreign key (contactfk) 
+       references actionexporter.contact;
