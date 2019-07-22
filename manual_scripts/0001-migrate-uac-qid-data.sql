b77fe57d2b619850140110e0b585d24f4552758b
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-07-22 15:21:54 +0100

    Fill in new event columns with data

--- manual_scripts/0001-migrate-uac-qid-data.sql
+++ manual_scripts/0001-migrate-uac-qid-data.sql
@@ -14,3 +14,5 @@ SELECT uac, qid, unique_number from casev2.uac_qid_link;
 SELECT setval('uacqid.uac_qid_unique_number_seq', COALESCE((SELECT last_value FROM casev2.uac_qid_link_unique_number_seq), 1), false);
 
 DROP SCHEMA iac CASCADE;
+
+UPDATE casev2.event SET event_channel = 'RM', event_source = 'CASE_SERVICE', event_transaction_id = uuid_generate_v4(), event_payload = '{}';
