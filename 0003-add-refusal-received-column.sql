0a32c5176fd902c02734131f1df15a55eec831d2
liamtoozer <liam.toozer@gmail.com>
2019-07-22 15:04:58 +0100

    Add refusal_received column

---
+++ 0003-add-refusal-received-column.sql
@@ -0,0 +1,14 @@
+-- ****************************************************************************
+-- *** MANUAL RM SQL DATABASE UPDATE SCRIPT                                 ***
+-- ****************************************************************************
+-- *** Number: 0003                                                         ***
+-- *** Purpose: Add new column 'refusal_received' in actionv2 & casev2      ***
+-- ***          cases tables to record refusals                             ***
+-- *** Author: Liam Toozer & Hugh Brace                                     ***
+-- ****************************************************************************
+
+ALTER TABLE casev2.cases
+ADD refusal_received BOOLEAN NOT NULL DEFAULT false;
+
+ALTER TABLE actionv2.cases
+ADD refusal_received BOOLEAN NOT NULL DEFAULT false;
