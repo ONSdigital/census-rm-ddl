1797cc0791a447fd5e5ec7a99647766265dfe72a
AdamHawtin <adam.hawtin@ons.gov.uk>
2020-01-20 15:50:26 +0000

    Tidy up patch script

--- groundzero_ddl/patch_version.sql
+++ groundzero_ddl/patch_version.sql
@@ -1,3 +1,3 @@
 CREATE schema patch_version;
 
-CREATE TABLE patch_version (patch_version integer PRIMARY KEY, applied_timestamp timestamp with time zone NOT NULL);
+CREATE TABLE patch_version (patch_number integer PRIMARY KEY, applied_timestamp timestamp with time zone NOT NULL);
--- patch_database.py
+++ patch_database.py
@@ -5,28 +5,34 @@ import psycopg2
 
 from config import Config
 
-conn = psycopg2.connect(f"dbname='{Config.DB_NAME}' user='{Config.DB_USERNAME}' host='{Config.DB_HOST}' "
+CONN = psycopg2.connect(f"dbname='{Config.DB_NAME}' user='{Config.DB_USERNAME}' host='{Config.DB_HOST}' "
                         f"password='{Config.DB_PASSWORD}' port='{Config.DB_PORT}'{Config.DB_USESSL}")
-conn.set_session(autocommit=False)
-cursor = conn.cursor()
+CONN.set_session(autocommit=False)
+CURSOR = CONN.cursor()
+
+patches_directory = Path(__file__).parent.joinpath('patches')
 
 
 def execute_sql_query(sql_query):
-    cursor.execute(sql_query)
-    return cursor.fetchall()
+    CURSOR.execute(sql_query)
+    return CURSOR.fetchall()
 
 
-patches_directory = Path(__file__).parent.joinpath('patches')
-sorted_patches = sorted([(patch_file, int(patch_file.name.split('_')[0])) for patch_file in patches_directory.iterdir()],
-                        key=lambda i: i[1])
-print([str(patch) for patch in sorted_patches])
+def get_current_patch_number():
+    return execute_sql_query('SELECT MAX(patch_version) FROM patch_version.patch_version;')[0][0] or 0
 
-current_patch = execute_sql_query('SELECT MAX(patch_version) FROM patch_version.patch_version;')[0][0] or 0
 
-patches_to_apply = [patch_file_version for patch_file_version in sorted_patches if patch_file_version[1] > current_patch]
+def patch_database():
+    patch_files = [{'file': patch_file, 'patch_number': int(patch_file.name.split('_')[0])} for patch_file in patches_directory.iterdir()]
+    current_patch = get_current_patch_number()
+    patches_to_apply = sorted([patch for patch in patch_files if patch['patch_number'] > current_patch], key=lambda patch: patch['patch_number'])
 
-for patch_file_version in patches_to_apply:
-    cursor.execute(patch_file_version[0].read_text())
-    cursor.execute(f"INSERT INTO patch_version.patch_version (patch_version, applied_timestamp)"
-                   f" VALUES ({patch_file_version[1]}, '{datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')}Z')")
-    conn.commit()
+    for patch in patches_to_apply:
+        CURSOR.execute(patch['file'].read_text())
+        CURSOR.execute(f"INSERT INTO patch_version.patch_version (patch_number, applied_timestamp)"
+                       f" VALUES (%integer, %s)", (patch['patch_number'], f'{datetime.utcnow().isoformat()}Z'))
+        CONN.commit()
+
+
+if __name__ == '__main__':
+    patch_database()
