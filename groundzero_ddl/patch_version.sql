58fbca61f654a1623f6f272133491c368d971246
liamtoozer <liam.toozer@gmail.com>
2020-01-21 14:44:42 +0000

    Wrap in try/catch and commit after all-or-nothing instead

--- groundzero_ddl/patch_version.sql
+++ groundzero_ddl/patch_version.sql
@@ -1,3 +1,3 @@
-CREATE schema patch_version;
+CREATE schema ddl_version;
 
-CREATE TABLE patch_version (patch_number integer PRIMARY KEY, applied_timestamp timestamp with time zone NOT NULL);
+CREATE TABLE ddl_version.patches (patch_number integer PRIMARY KEY, applied_timestamp timestamp with time zone NOT NULL);
--- patch_database.py
+++ patch_database.py
@@ -19,19 +19,28 @@ def execute_sql_query(sql_query):
 
 
 def get_current_patch_number():
-    return execute_sql_query('SELECT MAX(patch_version) FROM patch_version.patch_version;')[0][0] or 0
+    return execute_sql_query('SELECT MAX(patch_number) FROM ddl_version.patches')[0][0] or 0
 
 
 def patch_database():
-    patch_files = [{'file': patch_file, 'patch_number': int(patch_file.name.split('_')[0])} for patch_file in patches_directory.iterdir()]
+    patch_files = [{'file': patch_file, 'patch_number': int(patch_file.name.split('_')[0])} for patch_file in
+                   patches_directory.iterdir()]
     current_patch = get_current_patch_number()
-    patches_to_apply = sorted([patch for patch in patch_files if patch['patch_number'] > current_patch], key=lambda patch: patch['patch_number'])
+    patches_to_apply = sorted([patch for patch in patch_files if patch['patch_number'] > current_patch],
+                              key=lambda patch: patch['patch_number'])
 
     for patch in patches_to_apply:
-        CURSOR.execute(patch['file'].read_text())
-        CURSOR.execute(f"INSERT INTO patch_version.patch_version (patch_number, applied_timestamp)"
-                       f" VALUES (%integer, %s)", (patch['patch_number'], f'{datetime.utcnow().isoformat()}Z'))
-        CONN.commit()
+        try:
+            CURSOR.execute(patch['file'].read_text())
+            CURSOR.execute("INSERT INTO ddl_version.patches (patch_number, applied_timestamp)"
+                           " VALUES (%(patch_number)s, %(applied_timestamp)s)",
+                           {'patch_number': patch['patch_number'],
+                            'applied_timestamp': f'{datetime.utcnow().isoformat()}Z'})
+        except Exception:
+            CONN.rollback()
+            print(f"FAILURE ON PATCH: {patch['file'].name}")
+            raise
+    CONN.commit()
 
 
 if __name__ == '__main__':
