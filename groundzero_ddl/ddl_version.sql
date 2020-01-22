b0630d8896224d7060df13588f5b64c055ceb75a
AdamHawtin <adam.hawtin@ons.gov.uk>
2020-01-22 15:10:29 +0000

    Use arg parse, pass DB objects into functions, don't clone the repo for the DDL script

--- groundzero_ddl/ddl_version.sql
+++ groundzero_ddl/ddl_version.sql
@@ -1,3 +1,4 @@
 CREATE schema ddl_version;
 
 CREATE TABLE ddl_version.patches (patch_number integer PRIMARY KEY, applied_timestamp timestamp with time zone NOT NULL);
+CREATE TABLE ddl_version.version (version_tag varchar(20) PRIMARY KEY, updated_timestamp timestamp with time zone NOT NULL);
--- patch_database.py
+++ patch_database.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime
 from pathlib import Path
 
@@ -5,43 +6,72 @@ import psycopg2
 
 from config import Config
 
-CONN = psycopg2.connect(f"dbname='{Config.DB_NAME}' user='{Config.DB_USERNAME}' host='{Config.DB_HOST}' "
-                        f"password='{Config.DB_PASSWORD}' port='{Config.DB_PORT}'{Config.DB_USESSL}")
-CONN.set_session(autocommit=False)
-CURSOR = CONN.cursor()
-
-patches_directory = Path(__file__).parent.joinpath('patches')
+PATCHES_DIRECTORY = Path(__file__).parent.joinpath('patches')
 
 
-def execute_sql_query(sql_query):
-    CURSOR.execute(sql_query)
-    return CURSOR.fetchall()
+def get_current_patch_number(db_cursor):
+    db_cursor.execute('SELECT MAX(patch_number) FROM ddl_version.patches')
+    return db_cursor.fetchone()[0] or 0
 
 
-def get_current_patch_number():
-    return execute_sql_query('SELECT MAX(patch_number) FROM ddl_version.patches')[0][0] or 0
+def apply_patch(patch_file, patch_number, db_cursor, db_connection):
+    try:
+        db_cursor.execute(patch_file.read_text())
+        db_cursor.execute("INSERT INTO ddl_version.patches (patch_number, applied_timestamp)"
+                          " VALUES (%(patch_number)s, %(applied_timestamp)s)",
+                          {'patch_number': patch_number,
+                           'applied_timestamp': f'{datetime.utcnow().isoformat()}Z'})
+    except Exception:
+        db_connection.rollback()
+        print(f"FAILED ON PATCH: {patch_file.name}")
+        raise
 
 
-def patch_database():
-    patch_files = [{'file': patch_file, 'patch_number': int(patch_file.name.split('_')[0])} for patch_file in
+def update_patch_version(patch_version, db_cursor):
+    db_cursor.execute("INSERT INTO ddl_version.version (version, updated_timestamp)"
+                      " VALUES (%(version)s, %(updated_timestamp)s)",
+                      {'version': patch_version,
+                       'updated_timestamp': f'{datetime.utcnow().isoformat()}Z'})
+
+
+def patch_database(patches_directory, patch_version, db_cursor, db_connection):
+    patch_files = [{'file': patch_file, 'number': int(patch_file.name.split('_')[0])} for patch_file in
                    patches_directory.iterdir()]
-    current_patch = get_current_patch_number()
-    patches_to_apply = sorted([patch for patch in patch_files if patch['patch_number'] > current_patch],
-                              key=lambda patch: patch['patch_number'])
+    current_patch = get_current_patch_number(db_cursor)
+    patches_to_apply = sorted([patch for patch in patch_files if patch['number'] > current_patch],
+                              key=lambda patch: patch['number'])
 
     for patch in patches_to_apply:
-        try:
-            CURSOR.execute(patch['file'].read_text())
-            CURSOR.execute("INSERT INTO ddl_version.patches (patch_number, applied_timestamp)"
-                           " VALUES (%(patch_number)s, %(applied_timestamp)s)",
-                           {'patch_number': patch['patch_number'],
-                            'applied_timestamp': f'{datetime.utcnow().isoformat()}Z'})
-        except Exception:
-            CONN.rollback()
-            print(f"FAILURE ON PATCH: {patch['file'].name}")
-            raise
-    CONN.commit()
+        apply_patch(patch['file'], patch['number'], db_cursor, db_connection)
+    if patches_to_apply:
+        update_patch_version(patch_version, db_cursor)
+        db_connection.commit()
+        return
+    print(f'NO PATCHES TO APPLY AT VERSION: {patch_version}')
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Publish each file in a directory as a message to a rabbit queue')
+    parser.add_argument('-v',
+                        dest='patch_version',
+                        help='Semantic version of the DDL to patch to')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    with psycopg2.connect(f"dbname='{Config.DB_NAME}' "
+                          f"user='{Config.DB_USERNAME}' "
+                          f"host='{Config.DB_HOST}' "
+                          f"password='{Config.DB_PASSWORD}' "
+                          f"port='{Config.DB_PORT}"
+                          f"'{Config.DB_USESSL}") as db_connection:
+        db_connection.set_session(autocommit=False)
+        with db_connection.cursor() as db_cursor:
+            patch_database(PATCHES_DIRECTORY, args.patch_version, db_cursor, db_connection)
 
 
 if __name__ == '__main__':
-    patch_database()
+    main()
