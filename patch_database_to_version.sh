9bdf2b6c46f270a1e4dc7b07684a9e456041949a
liamtoozer <liam.toozer@gmail.com>
2020-01-27 12:57:59 +0000

    Checkout git branch

--- patch_database_to_version.sh
+++ patch_database_to_version.sh
@@ -3,4 +3,11 @@ if [ -z $PATCH_VERSION ]; then
   exit 1
 fi
 
-python patch_database.py $PATCH_VERSION
+rm -rf tmp_patch_database
+mkdir tmp_patch_database
+pushd tmp_patch_database
+
+
+git clone --branch $PATCH_VERSION https://github.com/ONSdigital/census-rm-ddl
+
+python patch_database.py
