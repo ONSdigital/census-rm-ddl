7000e0ef412154e7d030eea36dfc620b6c6fdc12
Ryan Grundy <ryan.grundy@ext.ons.gov.uk>
2020-01-31 15:10:53 +0000

    Added rebuild groundzero script

---
+++ groundzero_ddl/destroy_schemas.sql
@@ -0,0 +1,6 @@
+begin transaction;
+drop schema IF EXISTS casev2 cascade;
+drop schema IF EXISTS actionv2 cascade;
+drop schema IF EXISTS uacqid cascade;
+drop schema IF EXISTS ddl_version cascade;
+commit transaction;
