10a4468f55689b710a859fd5ed33dfd052266e00
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-02-27 14:09:21 +0000

    Bump version

--- groundzero_ddl/ddl_version.sql
+++ groundzero_ddl/ddl_version.sql
@@ -6,4 +6,4 @@ CREATE TABLE ddl_version.version (version_tag varchar(256) PRIMARY KEY, updated_
 -- Version and patch number for the current ground zero,
 -- NOTE: These must be updated every time the repo is tagged
 INSERT INTO ddl_version.patches (patch_number, applied_timestamp) VALUES (700, current_timestamp);
-INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.5.0', current_timestamp);
+INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.6.1', current_timestamp);
