1de6015b93f600900edd9d527d21dcd54e25aa61
Ryan Grundy <ryan.grundy@ons.gov.uk>
2020-03-19 16:21:07 +0000

    Remove create schema from ddl_version.sql

--- groundzero_ddl/ddl_version.sql
+++ groundzero_ddl/ddl_version.sql
@@ -1,5 +1,3 @@
-CREATE schema ddl_version;
-
 CREATE TABLE ddl_version.patches (patch_number integer PRIMARY KEY, applied_timestamp timestamp with time zone NOT NULL);
 CREATE TABLE ddl_version.version (version_tag varchar(256) PRIMARY KEY, updated_timestamp timestamp with time zone NOT NULL);
 
