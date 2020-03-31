7916d529c48610260a846197d95824a746521a19
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-03-31 14:42:10 +0100

    Remove ARID and replace estab ARID with estab UPRN

---
+++ patches/1200_drop_arid_rename_estab_column_to_uprn.sql
@@ -0,0 +1,17 @@
+ALTER table actionv2.cases
+DROP COLUMN arid;
+
+ALTER TABLE casev2.cases
+DROP COLUMN arid;
+
+ALTER table actionv2.cases
+DROP COLUMN estab_arid;
+
+ALTER TABLE casev2.cases
+DROP COLUMN estab_arid;
+
+ALTER TABLE actionv2.cases
+ADD COLUMN estab_uprn varchar(255);
+
+ALTER TABLE casev2.cases
+ADD COLUMN estab_uprn varchar(255);
