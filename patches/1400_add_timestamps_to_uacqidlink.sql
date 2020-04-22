59bde0daafe214f72266f62bb3dca7f9825a083b
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-04-22 15:14:45 +0100

    Use current_timestamp instead of now()

--- patches/1400_add_timestamps_to_uacqidlink.sql
+++ patches/1400_add_timestamps_to_uacqidlink.sql
@@ -5,9 +5,9 @@ ALTER TABLE casev2.uac_qid_link
     ADD COLUMN IF NOT EXISTS created_date_time timestamp with time zone;
 
 UPDATE casev2.uac_qid_link
-SET last_updated = now()
+SET last_updated = current_timestamp
 WHERE last_updated IS NULL;
 
 UPDATE casev2.uac_qid_link
-SET created_date_time = now()
+SET created_date_time = current_timestamp
 WHERE last_updated IS NULL;
