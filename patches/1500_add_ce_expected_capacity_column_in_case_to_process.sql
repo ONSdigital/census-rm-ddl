f4d53692e96e231d9a883d194805b864a85f9ce5
Leo Howard <38526889+LeoKHoward@users.noreply.github.com>
2020-04-29 08:49:34 +0100

    Update patches/1500_add_ce_expected_capacity_column_in_case_to_process.sql
    Co-Authored-By: Nick Grant <41681172+NickGrantONS@users.noreply.github.com>

--- patches/1500_add_ce_expected_capacity_column_in_case_to_process.sql
+++ patches/1500_add_ce_expected_capacity_column_in_case_to_process.sql
@@ -1,1 +1,1 @@
-ALTER TABLE actionv2.case_to_process ADD COLUMN IF NOT EXISTS ce_expected_capacity TYPE int;
+ALTER TABLE actionv2.case_to_process ADD COLUMN IF NOT EXISTS ce_expected_capacity TYPE int4;
