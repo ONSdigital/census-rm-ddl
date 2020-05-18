04a1f9a42dfdd44a8247b8a0d06110c05f510f14
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-05-18 12:46:11 +0100

    Update 1600_add_extra_case_event_and_uacqid_indexes.sql

--- patches/1600_add_extra_case_event_and_uacqid_indexes.sql
+++ patches/1600_add_extra_case_event_and_uacqid_indexes.sql
@@ -1,3 +1,3 @@
-create index IF NOT EXISTS casev2.event_uac_qid_link_id on event (uac_qid_link_id);
-create index IF NOT EXISTS casev2.event_caze_case_id on event (caze_case_id);
-create index IF NOT EXISTS casev2.uac_qid_caze_case_id on uac_qid_link (caze_case_id);
+create index IF NOT EXISTS event_uac_qid_link_id on casev2.event (uac_qid_link_id);
+create index IF NOT EXISTS event_caze_case_id on casev2.event (caze_case_id);
+create index IF NOT EXISTS uac_qid_caze_case_id on casev2.uac_qid_link (caze_case_id);
