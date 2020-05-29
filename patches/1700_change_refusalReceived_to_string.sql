323f37d28f7ea1ed617ba37d21fd6c234242f0e1
LeoKHoward <leo-howard@hotmail.co.uk>
2020-05-29 08:42:13 +0100

    Patch

--- patches/1700_change_refusalReceived_to_string.sql
+++ patches/1700_change_refusalReceived_to_string.sql
@@ -1,3 +1,3 @@
-ALTER TABLE actionv2.cases ALTER COLUMN refusal_received varchar(255) NOT NULL;
+ALTER TABLE actionv2.cases ALTER COLUMN refusal_received TYPE varchar(255);
 
 ALTER TABLE casev2.cases ALTER COLUMN refusal_received TYPE varchar(255);
