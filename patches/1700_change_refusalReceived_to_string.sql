83a34161b4b7d38c975fc6f674f933c1ef3886eb
LeoKHoward <leo-howard@hotmail.co.uk>
2020-05-29 09:26:27 +0100

    Patch changes

--- patches/1700_change_refusalReceived_to_string.sql
+++ patches/1700_change_refusalReceived_to_string.sql
@@ -1,3 +1,11 @@
-ALTER TABLE actionv2.cases ALTER COLUMN refusal_received TYPE varchar(255);
-
 ALTER TABLE casev2.cases ALTER COLUMN refusal_received TYPE varchar(255);
+ALTER TABLE casev2.cases ALTER COLUMN refusal_received DROP NOT NULL;
+ALTER TABLE casev2.cases ALTER COLUMN refusal_received DROP DEFAULT;
+UPDATE casev2.cases SET refusal_received = null WHERE refusal_received = 'false';
+UPDATE casev2.cases SET refusal_received = 'HARD_REFUSAL' WHERE refusal_received = 'true';
+
+ALTER TABLE actionv2.cases ALTER COLUMN refusal_received TYPE varchar(255);
+ALTER TABLE actionv2.cases ALTER COLUMN refusal_received  DROP NOT NULL;
+ALTER TABLE actionv2.cases ALTER COLUMN refusal_received DROP DEFAULT;
+UPDATE actionv2.cases SET refusal_received = null WHERE refusal_received = 'false';
+UPDATE actionv2.cases SET refusal_received = 'HARD_REFUSAL' WHERE refusal_received = 'true';
