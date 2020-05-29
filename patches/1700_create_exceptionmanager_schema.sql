ec0928c7cd4c6454e7a35a48b858a62e0503d41c
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-05-29 13:07:52 +0100

    Add patch script

---
+++ patches/1700_create_exceptionmanager_schema.sql
@@ -0,0 +1,16 @@
+create schema if not exists exceptionmanager;
+set schema exceptionmanager;
+create table quarantined_message (
+   id uuid not null,
+    content_type varchar(255),
+    error_reports jsonb,
+    headers jsonb,
+    message_hash varchar(255),
+    message_payload bytea,
+    queue varchar(255),
+    routing_key varchar(255),
+    service varchar(255),
+    skipped_timestamp timestamp with time zone,
+    primary key (id)
+);
+
