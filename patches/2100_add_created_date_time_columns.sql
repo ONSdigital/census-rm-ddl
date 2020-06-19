054e091c98d4dd4b5a50080aafae7f7fa0782177
LeoKHoward <leo-howard@hotmail.co.uk>
2020-06-19 08:13:44 +0100

    Removal of duplicated patch

--- patches/2100_add_created_date_time_columns.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE actionv2.cases ADD COLUMN IF NOT EXISTS created_date_time timestamp with time zone;
-
-ALTER TABLE actionv2.cases ADD COLUMN IF NOT EXISTS last_updated timestamp with time zone;
