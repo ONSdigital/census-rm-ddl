84a1f5a4dc3aadc72877c090517da1094950179e
LeoKHoward <leo-howard@hotmail.co.uk>
2020-06-19 10:59:28 +0100

    Setting default values of fields if null

--- patches/2200_add_created_date_time_columns.sql
+++ patches/2200_add_created_date_time_columns.sql
@@ -1,3 +1,7 @@
 ALTER TABLE actionv2.cases ADD COLUMN IF NOT EXISTS created_date_time timestamp with time zone;
 
 ALTER TABLE actionv2.cases ADD COLUMN IF NOT EXISTS last_updated timestamp with time zone;
+
+UPDATE actionv2.cases AC SET created_date_time = CC.created_date_time FROM casev2.cases CC WHERE CC.case_id = AC.case_id AND AC.created_date_time is null;
+
+UPDATE actionv2.cases AC SET last_updated = CC.last_updated FROM casev2.cases CC WHERE CC.case_id = AC.case_id AND AC.last_updated is null;
