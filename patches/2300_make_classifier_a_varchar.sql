2e2410c44068eeb295ba486f8e1cac9b96aff75d
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2020-07-03 12:59:19 +0100

    db fix

--- patches/2300_make_classifier_a_varchar.sql
+++ patches/2300_make_classifier_a_varchar.sql
@@ -5,4 +5,4 @@ ALTER TABLE actionv2.action_rule
     DROP COLUMN classifiers;
 
 ALTER TABLE actionv2.action_rule
-    ADD COLUMN IF NOT EXISTS user_defined_where_clause VARCHAR(255) NOT NULL;
+    ADD COLUMN IF NOT EXISTS user_defined_where_clause VARCHAR(100000) NOT NULL;
