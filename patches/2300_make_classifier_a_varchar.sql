29fffc174261f8ecee133ad5b5db4968e67df1fb
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2020-07-07 14:03:04 +0100

    classifier_clause column to TEST type

--- patches/2300_make_classifier_a_varchar.sql
+++ patches/2300_make_classifier_a_varchar.sql
@@ -1,8 +1,4 @@
 
 TRUNCATE actionv2.action_rule CASCADE;
-
-ALTER TABLE actionv2.action_rule
-    DROP COLUMN classifiers;
-
-ALTER TABLE actionv2.action_rule
-    ADD COLUMN IF NOT EXISTS user_defined_where_clause VARCHAR(100000) NOT NULL;
+ALTER TABLE actionv2.action_rule DROP COLUMN classifiers;
+ALTER TABLE actionv2.action_rule ADD COLUMN IF NOT EXISTS classifiers_clause TEXT NOT NULL;
