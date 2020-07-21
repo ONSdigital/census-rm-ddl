aa91a4e6ddf376a6071fbe526373b597000484e9
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2020-07-21 14:50:41 +0100

    Spike multiple SQL instances (#67)

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+ACTION_CERTS = (" sslmode=verify-ca sslrootcert=/home/toolbox/.postgresql-action/root.crt "
+                "sslcert=/home/toolbox/.postgresql-action/postgresql.crt "
+                "sslkey=/home/toolbox/.postgresql-action/postgresql.key")
+
 
 class Config:
     DB_USERNAME = os.getenv('DB_USERNAME', 'postgres')
@@ -8,3 +12,5 @@ class Config:
     DB_PORT = os.getenv('DB_PORT', '6432')
     DB_NAME = os.getenv('DB_NAME', 'postgres')
     DB_USESSL = os.getenv('DB_USESSL', '')
+    DB_HOST_ACTION = os.getenv('DB_HOST_ACTION', 'localhost')
+    DB_ACTION_CERTIFICATES = os.getenv('DB_ACTION_CERTIFICATES', ACTION_CERTS)
