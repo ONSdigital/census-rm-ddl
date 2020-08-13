dcd8af75a042eb0365f20f3c95aec3e4b99f8b39
LeoKHoward <leo-howard@hotmail.co.uk>
2020-08-13 11:56:08 +0100

    Unbumped version

--- patch_database.py
+++ patch_database.py
@@ -9,10 +9,10 @@ PATCHES_DIRECTORY = Path(__file__).parent.joinpath('patches/main')
 PATCHES_DIRECTORY_ACTION = Path(__file__).parent.joinpath('patches/action')
 
 # current_version should match the version in the ddl_version.sql file
-current_version = 'v4.3.0'
+current_version = 'v4.2.0'
 
 # current_version_action should match the version in the ACTION-ddl_version.sql file
-current_version_action = 'v1.2.0'
+current_version_action = 'v1.1.0'
 
 
 def get_current_patch_number(db_cursor):
