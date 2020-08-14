f153194d7a92f47be1134037bc6b4c014e3834b0
Leo Howard <38526889+LeoKHoward@users.noreply.github.com>
2020-08-14 10:21:36 +0100

    New permissions file and inclusion of exception manager (#70)
    * New permissions file and inclusion of exception manager
    
    * Removal of patches and relocation of permissions
    
    * remove pycache
    
    * Unbumped version

--- permissions/RM-Read-Action-DB-Permissions.sql
+++ permissions/RM-Read-Action-DB-Permissions.sql
@@ -1,32 +1,25 @@
 -- ********************************************
 -- *** MANUAL RM SQL DATABASE UPDATE SCRIPT ***
 -- ********************************************
--- *** Number: 0010                         ***
--- *** Purpose: Create and grant            *** 
--- *** permissions on rm read only access   ***
--- *** role                                 ***
--- *** Author: Adam Hawtin                  ***
+-- *** Number: 0001                         ***
+-- *** Purpose: Create and grant            ***
+-- *** permissions on rm Action DB          ***
+-- *** read only access role                ***
+-- ***                                      ***
+-- *** Author: Leo Howard                   ***
 -- ********************************************
 
 CREATE ROLE rm_read_access;
 
 GRANT CONNECT ON DATABASE rm TO rm_read_access;
 
-GRANT USAGE ON SCHEMA casev2 TO rm_read_access;
 GRANT USAGE ON SCHEMA actionv2 TO rm_read_access;
-GRANT USAGE ON SCHEMA uacqid TO rm_read_access;
 GRANT USAGE ON SCHEMA ddl_version TO rm_read_access;
 
-GRANT SELECT ON ALL TABLES IN SCHEMA casev2 TO rm_read_access;
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA casev2 TO rm_read_access;
 GRANT SELECT ON ALL TABLES IN SCHEMA actionv2 TO rm_read_access;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA actionv2 TO rm_read_access;
-GRANT SELECT ON ALL TABLES IN SCHEMA uacqid TO rm_read_access;
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA uacqid TO rm_read_access;
 GRANT SELECT ON ALL TABLES IN SCHEMA ddl_version TO rm_read_access;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA ddl_version TO rm_read_access;
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA casev2 GRANT SELECT ON TABLES TO rm_read_access;
 ALTER DEFAULT PRIVILEGES IN SCHEMA actionv2 GRANT SELECT ON TABLES TO rm_read_access;
-ALTER DEFAULT PRIVILEGES IN SCHEMA uacqid GRANT SELECT ON TABLES TO rm_read_access;
 ALTER DEFAULT PRIVILEGES IN SCHEMA ddl_version GRANT SELECT ON TABLES TO rm_read_access;
