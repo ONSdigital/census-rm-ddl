3348c4aeb903299821602366ccb11672f2a47e1e
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-09-07 11:34:03 +0100

    Add new columns for Exception Manager rules

--- groundzero_ddl/exceptionmanager.sql
+++ groundzero_ddl/exceptionmanager.sql
@@ -2,6 +2,10 @@
     create table auto_quarantine_rule (
        id uuid not null,
         expression varchar(255),
+        quarantine BOOLEAN DEFAULT false not null,
+        rule_expiry_date_time timestamp with time zone,
+        suppress_logging BOOLEAN DEFAULT false not null,
+        throw_away BOOLEAN DEFAULT false not null,
         primary key (id)
     );
 
