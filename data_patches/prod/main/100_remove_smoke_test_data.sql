5cf18721c76851f2939f6e3f30a3b36fe5cacadd
davidmort <davidmort100@gmail.com>
2020-10-27 09:17:16 +0000

    created data patch folder with a patch to remove smoke test data from prod

---
+++ data_patches/prod/main/100_remove_smoke_test_data.sql
@@ -0,0 +1,3 @@
+DELETE from casesvc.events where id in ('f71f8908-38b4-4f60-b87b-d3a0ea5cfb70','58e94664-a3ea-4b06-b292-613f99f32630');
+
+DELETE from casesvc.uac_qid_link where id in ('ba168af7-ee3b-40f8-8b5e-16c53ce7ee50','39325386-0ea4-4c2a-80ed-59c19fa298d5')
