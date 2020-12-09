e2a6c9c7cc812c6874ec344fb9ff8747334def8d
Ryan Grundy <ryan.grundy@ons.gov.uk>
2020-12-09 13:06:26 +0000

    Now just a single script which passes in an sql file

--- run-sql-file-to-case-db.sh
+++ run-sql-file-to-case-db.sh
@@ -1,5 +1,7 @@
-echo "Removing uprn index from database at $(date -u +"%FT%H-%M-%S")"
+set -e
+
+echo "Running SQL file: $SQL_FILE"
 PSQL_CONNECT_WRITE_MODE="sslmode=verify-ca sslrootcert=/root/.postgresql/root.crt sslcert=/root/.postgresql/postgresql.crt sslkey=/root/.postgresql/postgresql.key hostaddr=$DB_HOST user=rmuser password=${PASSWORD:=password} dbname=$DB_NAME"
 
-psql "$PSQL_CONNECT_WRITE_MODE" -f data_patches/prod/main/300_remove_uprn_index_on_cases.sql
-echo "Index has been removed at $(date -u +"%FT%H-%M-%S")"
+psql "$PSQL_CONNECT_WRITE_MODE" -f "$SQL_FILE"
+echo "Finished running file at $(date -u +"%FT%H-%M-%S")"
